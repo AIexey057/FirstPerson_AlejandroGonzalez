0d69a3ef03b6b0a16da0741e7fd04ae9b322722a
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-10-28 17:04:52 +0100

    mam

--- Assets/Scripts/FistPerson.cs
+++ Assets/Scripts/FistPerson.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine; 
 
 public class FistPerson : MonoBehaviour
 {
-    [SerializeField] private float velocidadMovimiento;
+    [Header("Detteccion del suelo")]
+   
+   [SerializeField] private Transform pies;
+   [SerializeField] private float radioDetteccion;
+   [SerializeField] private LayerMask queEsSuelo;
+    
+    [Header("Movimiento")]
+    
+   [SerializeField] private float velocidadMovimiento;
+   [SerializeField] private float escalaGravedad;
+   [SerializeField] private float alturaSalto;
 
     CharacterController controler;
+    private Vector3 movimientoVertical;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +34,7 @@ public class FistPerson : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
 
         Vector2 input = new Vector2(h, v).normalized;
-        if (input.magnitude > 0)
+        if (input.sqrMagnitude > 0)
         {
             float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
             transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
@@ -30,9 +42,42 @@ public class FistPerson : MonoBehaviour
             Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) *  Vector3.forward;
            
             controler.Move(movimiento * velocidadMovimiento * Time.deltaTime);
-        }   
-       
-       
+        }
+        Aplicargravedad();
+        DeteccionSuelo();        
+
         
+
+    }
+    private void Aplicargravedad()
+    {
+        movimientoVertical.y += escalaGravedad * Time.deltaTime;
+        controler.Move(movimientoVertical * Time.deltaTime);
+    }
+
+    private void DeteccionSuelo()
+    {
+      Collider[] collsDetectados =  Physics.OverlapSphere(pies.position, radioDetteccion, queEsSuelo);
+        if (collsDetectados.Length > 0)
+        {
+            movimientoVertical.y = 0;
+            Salto();
+        }
+
+    }
+    
+    private void Salto()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movimientoVertical.y = Mathf.Sqrt(-2 * escalaGravedad * alturaSalto);
+        }
+    }
+    
+        
+        private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(pies.position, radioDetteccion);
     }
 }
