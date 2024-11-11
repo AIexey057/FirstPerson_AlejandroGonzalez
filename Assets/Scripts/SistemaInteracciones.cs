8147496a34c638cacbc03ed67ae8fd819c158b1c
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-11-11 18:13:16 +0100

    11/11/24

---
+++ Assets/Scripts/SistemaInteracciones.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SistemaInteracciones : MonoBehaviour
+{
+    
+    private Camera cam;
+    [SerializeField] private float distanciaDeteccion;
+
+    private Transform interactuableActual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+      
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, distanciaDeteccion))
+        {
+
+            if (hit.transform.CompareTag("Interactuable"))
+            {
+                interactuableActual = hit.transform;
+                interactuableActual.GetComponent<Outline>().enabled = true;
+            }
+            
+        }
+        else if (interactuableActual)
+        {
+            interactuableActual.GetComponent<Outline>().enabled = false;
+
+            interactuableActual = null;
+        }
+    }
+    
+}
